6e6f3523306628a2e34fda2619b133b08ffce617
Rainer_Dorn <Rainer_Dorn@gmx.net>
2017-05-09 15:36:00 +0200

    Task2

---
+++ lessons/Lesson2_dorn/Lesson2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    class Book
+    {
+        private string title;
+        private decimal price;
+        public Book(string newTitle, decimal newPrice)
+        {
+
+            public string GetTitle(string newTitle)
+            {
+            if (newTitle == null || newTitle.Length == 0)
+                throw new ArgumentException("Kein Titel!");
+            return title;
+            }
+        public decimal GetPrice()
+            { return price; }
+        public void SetTitle(decimal newPrice)
+        {
+            if (newPrice < 0)
+                throw new ArgumentException("Negativer Preis!");
+            price = newPrice;
+
+        }
+
+      
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Konstruktoroperationen heißen wie die Klasse selber
+            Book a = new Book("Mein erstes Buch", 10);
+            //Book b = a;
+            //a.price = -10;
+            //Console.WriteLine("Book.price: " + a.price);
+
+
+            
+        }
+    }
+}
---
+++ tasks/Task2/Task2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Net;
+
+namespace Task2
+{
+    public class Verein
+    {
+        //private int m_alter;
+
+        public Verein(string newName, string newInstrument)
+        {
+            if (string.IsNullOrWhiteSpace(newName)) throw new ArgumentException("Name darf nicht leer sein.", nameof(newName));
+            if (string.IsNullOrWhiteSpace(newInstrument)) throw new ArgumentException("Alter muss > 0 sein.", nameof(newAlter));
+
+            Name = newName;
+            Instrument = newInstrument;
+            UpdateAlter(m_alter);
+
+
+        }
+
+        public string Name { get; }
+        public string Instrument { get; }
+        public int  m_alter { get; private set; }
+
+        static void Main(string[] args)
+        {
+            var verein = new[]
+            {
+                new Verein("Dorn", "Klarinette"),
+                new Verein("Kuestner", "Fluegelhorn"),
+                new Verein("Mauser", "Tuba"),
+
+            };
+
+            foreach (var b in verein)
+            {
+                Console.WriteLine(b.Name, b.Instrument, b.m_alter);
+            }
+        }
+
+        public void UpdateAlter(int newAlter)
+        {
+            if (newAlter <= 0) throw new ArgumentException("Alter darf nicht 0 sein.", nameof(newAlter));
+            m_alter = newAlter;
+
+        }
+    }
+}
+
